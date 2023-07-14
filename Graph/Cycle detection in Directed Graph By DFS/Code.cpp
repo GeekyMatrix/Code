6ce58007a607bb8b9a2842d3d43d0b888d232c77
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-14 10:21:17 +0530

    Update Code.cpp

--- Graph/Cycle detection in Directed Graph By DFS/Code.cpp
+++ Graph/Cycle detection in Directed Graph By DFS/Code.cpp
@@ -1,1 +1,34 @@
-
+class Solution {
+  public:
+    
+    bool isCycleDFS(vector<int> adj[], int node, vector<bool>& visited, vector<bool>& dfsVisited) {
+        visited[node] = true;
+       dfsVisited[node] = true;
+        
+        
+        for(int &neighbour : adj[node]) {
+            //if not visited, then we check for cycle in DFS
+            if(visited[neighbour] == false && isCycleDFS(adj, v, visited, dfsVisited))
+                return true;
+            else if(dfsVisited[neighbour] == true)
+                return true;
+        }
+        
+       dfsVisited[node] = false;
+        return false;
+        
+    }
+    
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<bool> visited(V, false);
+        vector<bool> dfsVisited(V, false);
+        
+        for(int i = 0; i<V; i++) {
+            if(!visited[i] && isCycleDFS(adj, i, visited, dfsVisited))
+                return true;
+        }
+        
+        return false;
+    }
+};
