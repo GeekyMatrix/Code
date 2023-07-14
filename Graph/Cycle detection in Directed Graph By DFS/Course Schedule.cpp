851e130830dedd2cf96b706ff7b91c71325f25d2
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-14 10:36:00 +0530

    Create Course Schedule.cpp

---
+++ Graph/Cycle detection in Directed Graph By DFS/Course Schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+//Cycle detection in directed graph
+   bool Dfs(int s, vector<bool>&visited, vector<bool>&currvisited,vector<int>adj[]){
+  visited[s]=true;
+  currvisited[s]=true;
+  for(auto neighbour:adj[s]){
+      if(!visited[neighbour]) {
+          if(Dfs(neighbour,visited,currvisited,adj)) return true;
+      }
+      else if(visited[neighbour]&&currvisited[neighbour]) return true;
+  }
+   currvisited[s]=false;
+   return false;
+   }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<bool>Visited(n,false);
+        vector<bool>currVisited(n,false);
+    //MAKING ADJACENT LIST
+       vector<int>adj[n];
+        for(auto x:prerequisites){
+        vector<int>data=x;
+        int a=data[0];
+        int b=data[1];
+        adj[a].push_back(b);
+        }
+    //Dfs call 
+    for(int i=0;i<n;i++){
+        if(!Visited[i]){
+            if(Dfs(i,Visited,currVisited,adj)) return false;
+    }
+    }
+    return true;
+    }
+};
