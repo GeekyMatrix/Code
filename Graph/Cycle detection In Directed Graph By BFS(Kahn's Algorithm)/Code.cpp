3d9e38a75fae3249ac806e9d10eb80f23a697c49
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-14 10:31:46 +0530

    Update Code.cpp

--- Graph/Cycle detection In Directed Graph By BFS(Kahn's Algorithm)/Code.cpp
+++ Graph/Cycle detection In Directed Graph By BFS(Kahn's Algorithm)/Code.cpp
@@ -1,1 +1,41 @@
-
+class Solution {
+  public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int N, vector<int> adj[]) {
+        queue<int> que;
+	    vector<int> indegree(N, 0);
+	    int count = 0;
+	    //1 Sbka indegree find karo
+	    for(int u = 0; u<N; u++) {
+	        for(int &v : adj[u]) {
+	            indegree[v]++;
+	        }
+	    }
+	    
+	    //2. Fill que, indegree with 0
+	    for(int i = 0; i<N; i++) {
+	        if(indegree[i] == 0) {
+	            que.push(i);
+	            count++;
+	        }
+	    }
+	    
+	    //3. Simple BFS
+	    while(!que.empty()) {
+	        int front = que.front();
+	        que.pop();
+	        
+	        for(int &neighbour : adj[front]) {
+	            indegree[neighbour]--;
+	            
+	            if(indegree[neighbour] == 0) {
+	                que.push(neighbour);
+	                count++;
+	            }
+	            
+	        }
+	    }
+	    
+	    return count != N;
+    }
+};
