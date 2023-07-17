a3d7a46c1babced0701461ae40f3233e527d438f
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-17 17:12:41 +0530

    Update Reverse LL.cpp

--- Linked List/Reverse LL.cpp
+++ Linked List/Reverse LL.cpp
@@ -1,1 +1,25 @@
+//ITERATIVE APPROACH
+ ListNode*reverseLL(ListNode*head){
+          ListNode *new_head=NULL;
+          while(head!=NULL){
+            ListNode *curr=head;
+            head=head->next;
+            curr->next=new_head;
+            new_head=curr;
+          } 
+         return new_head;
+     }
 
+//RECURSIVE METHOD
+ ListNode*reverseLL(ListNode*head){
+         //BASE CASE
+         if(head==NULL||head->next==NULL) return head;
+
+         //Recursive call
+         ListNode*SmallAns=reverseLL(head->next);
+         //Calculation
+        head->next->next=head;
+         head->next=NULL;
+         return SmallAns;
+
+     }
