643b80a7592e7a4170bba76a1e7f3b498ecf4449
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-18 10:13:00 +0530

    Create Shortest Path in Binary Matrix.cpp

---
+++ Graph/Dijkstra's Shortest path/Shortest Path in Binary Matrix.cpp
@@ -0,0 +1,45 @@
+
+//APPROACH 2 (BY Dijkstras Method)
+int m,n;
+  vector<vector<int>>directions{{1,1},{-1,-1},{0,1},{0,-1},{1,0},{-1,0},{1,-1},{-1,1}}; 
+
+  bool isSafe(int x,int y){
+         if(x>=0 && x<m && y>=0 && y<n) return true;
+         return false;
+  }
+
+typedef pair<int,pair<int,int>>p; // distance,(x,y);
+
+int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+         m=grid.size();
+         n=grid[0].size();
+   if(m == 0 || n == 0 || grid[0][0] != 0) return -1;
+vector<vector<int>>result(m,vector<int>(n,INT_MAX));
+
+priority_queue<p,vector<p>,greater<p>>pq;
+pq.push({0,{0,0}});
+result[0][0]=0;
+
+while(!pq.empty()){
+ int d=pq.top().first;
+ pair<int,int>node=pq.top().second;
+ pq.pop();
+ int x=node.first;
+ int y=node.second;
+
+ for(auto dir:directions){
+     int x_ =x+dir[0];
+     int y_ =y+dir[1];
+
+     int dist=1;
+
+     if(isSafe(x_,y_) && grid[x_][y_]==0 && d+dist<result[x_][y_]){
+        pq.push({d+dist,{x_,y_}});
+        grid[x_][y_]=1;
+         result[x_][y_]=d+dist;
+     }
+ }
+}
+  if(result[m-1][n-1]==INT_MAX) return -1;
+  return result[m-1][n-1]+1;
+}
