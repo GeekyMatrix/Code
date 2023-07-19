070a9ef18499e2f9fc98892d20fdbcbda20438f5
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-19 17:54:35 +0530

    Update Code.cpp

--- Graph/Kruskal's Algorithm(DSU)/Code.cpp
+++ Graph/Kruskal's Algorithm(DSU)/Code.cpp
@@ -1,60 +1,94 @@
-//Code
-bool cmp(vector<int>&a,vector<int>&b){
- return a[2]<b[2];
-}
+class Solution
+{
+	public:
+    //DSU Code
+	vector<int> parent;
+    vector<int> rank;
+    
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+    
+        return parent[x] = find(parent[x]);
+    }
+    
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+    
+        if (x_parent == y_parent) 
+            return;
+    
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    
+    int Kruskal(vector<vector<int>> &vec) {
 
-void makeset(vector<int>&parent,vector<int>&rank,int n){
- for(int i=0;i<n;i++){
-  parent[i]=i;
-   rank[i]=0;
- }
-}
+        int sum = 0;
+        for(auto &temp : vec) {
+            
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
+            
+            int parent_u = find(u);
+            int parent_v = find(v);
+            
+            if(parent_u != parent_v) {
+                Union(u, v);
+                sum += wt;
+            }
+            
+        }
 
-//Find parent using path compression
-int findparent(vector<int>&parent,int node){
- if(parent[node]==node) return node;
+        return sum;
+    }
+    
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        
 
- return parent[node]=findparent(parent,parent[node]);
-}
+        parent.resize(V);
+	    rank.resize(V, 0);
+        	    
+	    for(int i = 0; i<V; i++)
+	        parent[i] = i;
 
-//Union Find
-void UnionSet(int u,int v,vector<int>&parent,vector<int>&rank){
- u=findparent(parent,u); 
- v=findparent(parent,v); 
+        vector<vector<int>> vec;
+        
+        
+        for(int i = 0; i<V; i++) {
+            
+            for(auto &temp : adj[i]) {
+                
+                int u = i;
+                int v = temp[0];
+                int d = temp[1];
 
-  if(rank[u]<rank[v]) parent[u]=v;
-
-  else if(rank[v]<rank[u]) parent[v]=u;
-
-  else{
-      parent[v]=u;
-    rank[u]++;
-  }
-}
-
-///////
-int minimunspanningTree(vector<vector<int>>&edges,int n){
-
-sort(edges.begin(),edges.end(),cmp);
-  vector<int>parent(n);
-  vector<int>rank(n);
-
-  makeset(parent,rank,n);
-  int minweight=0;
-
-for(int i=0;i<edges.size();i++){
-int u=findparent(parent,edges[i][0];
-int v=findparent(parent,edges[i][1];
-  int wt=edges[i][2];
-
-  if(u!=v){
-   minweight+=wt;
-    unionset(u,v,parent,rank);
-  }
-}
-return minweight;
-}
-
+                vec.push_back({u, v, d});
+                
+            }
+            
+        }
+        
+        
+        auto lambda = [&](auto &v1, auto &v2) {
+            return v1[2] < v2[2];
+        };
+        
+        sort(begin(vec), end(vec), lambda);
+        
+        return Kruskal(vec);
+        
+    }
+};
 
 
 
