797b9cefd5188df4cc6faf57124155823cc3742f
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-20 21:05:03 +0530

    Create QuickSort.cpp

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,40 @@
+int partition(int arr[],int s,int e){
+ int pivot=arr[s];
+  int cnt=0;
+  for(int i=s+1;i<=e;i++){
+   if(arr[i]<=pivot) cnt++;
+  }
+//Place pivot at right position
+  int pivotIndex=s+cnt;
+  swap(arr[pivotIndex],arr[s]);
+//left and right wale part ko smbhal lete hai
+  int i=s,j=e;
+  while(i<pivotIndex && j>pivotIndex){
+  while(arr[i]<=pivot) i++;
+  while(arr[j]>pivot)  j--;
+    if(i<pivotIndex && j>pivotIndex) swap(arr[i++],arr[j--]);
+  }
+  return  pivotIndex;
+}
+
+Void quickSort(int arr[],int s,int e){
+//BASE CASE
+  if(s>=e) return;
+//Partition karenge
+  int p=partition(arr,s,e);
+  //left Part Sort karo
+  quickSort(arr,s,p-1);
+  //Right part Sort karo
+ quickSort(arr,p+1,e);
+}
+
+int main(){
+int arr[10]={2,4,1,6,9,9,9,9,9,9};
+  int n=10;
+  quickSort(arr,0,n-1);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<"";
+  }
+  cout<<endl;
+  return 0;
+}
