3b969e6ec2ce1340efdbc64bfe8b5edfcd647dc9
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-21 17:26:10 +0530

    Update Count the no. of Subset with a Given Difference.cpp

--- DP/0,1Knapsack/Count the no. of Subset with a Given Difference.cpp
+++ DP/0,1Knapsack/Count the no. of Subset with a Given Difference.cpp
@@ -1,1 +1,12 @@
+/* arr[]={1,1,2,3}
+diff=1
+o/p=2
 
+sum[s1]-sum[s2]=diff;  eqn 1)  // s1,s2 is a subarray
+sum[s1]+sum[s2]=sum[arr];  eqn 2)
+
+add eqn1&2
+
+sum[s1]=(diff+sum[arr])/2;
+*/
+return count of subsetSum(arr,sum[s1]);
