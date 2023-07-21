de319ac766c5d9c6a349bd4990af1238c5b57570
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-21 16:44:44 +0530

    Update Minimum Subset Sum Difference.cpp

--- DP/0,1Knapsack/Minimum Subset Sum Difference.cpp
+++ DP/0,1Knapsack/Minimum Subset Sum Difference.cpp
@@ -1,1 +1,15 @@
+/* i/p ->arr[]: 1,6,11,5
+{1,6,5}           {11} 
+diff=1
+S1-S2==Minm;
+*/
 
+//CODE
+Subset Sum(int arr[],int Range){
+
+ //write code
+
+int mn=INT_MAX;
+  for(int i=0;i<v.size();i++) mn=min(mn,Range-2v[i]);
+return mn;
+}
