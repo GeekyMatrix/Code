09aebe92c9a97934ac549e03b8c16d0414f2c918
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-23 21:50:46 +0530

    Update Mid of  a LL.cpp

--- Linked List/Mid of  a LL.cpp
+++ Linked List/Mid of  a LL.cpp
@@ -1,1 +1,13 @@
+// Mid find karna
+Node *findMid(Node *head){
+ if(head==NULL ||head->next==NULL) return head;
 
+Node *slow=head;
+  Node *fast=head->next;
+
+while(fast!=NULL && fast->next!=NULL){
+slow=slow->next;
+  fast=fast->next->next;
+}
+return slow;
+}
