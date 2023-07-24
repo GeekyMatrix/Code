b42ff62a24ded4eb87b8d599ad13adc120bb22ed
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-24 09:28:53 +0530

    Create Create a Binary  Tree Node.cpp

---
+++ Binary Tree/Create a Binary  Tree Node.cpp
@@ -0,0 +1,11 @@
+int main(){
+BinaryTreeNode<int> *root=new BinaryTreeNode<int>(1);
+
+BinaryTreeNode<int> *node1=new BinaryTreeNode<int>(2);
+
+BinaryTreeNode<int> *node2=new BinaryTreeNode<int>(3);
+
+
+root->left=node1;
+root->right=node2;
+}
