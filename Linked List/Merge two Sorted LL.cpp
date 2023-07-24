f2b1b96efd56812d6a462ff5b7712ce4708cf417
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-24 08:58:08 +0530

    Update Merge two Sorted LL.cpp

--- Linked List/Merge two Sorted LL.cpp
+++ Linked List/Merge two Sorted LL.cpp
@@ -1,1 +1,43 @@
+Node *merge(Node *head1,Node *head2){
 
+  if(head1==NULL) return head2;
+   if(head2==NULL) return head1;
+
+  Node *head=NULL;
+  Node *Tail=NULL;
+
+  if(head1->data <head2->data){
+  head=head1;
+  Tail=head1;
+  head1=head1->next;
+  }
+
+else{
+ head=head2;
+  Tail=head2;
+  head2=head2->next;
+}
+
+while(head1!=NULL && head2!=NULL){
+ if(head1->data < head2->data){
+ Tail->next=head1;
+  Tail=Tail->next;
+   head1=head1->next;
+ }
+else{
+ Tail->next=head2;
+  Tail=Tail->next;
+   head2=head2->next;
+}
+}
+  if(head1!=NULL){
+ Tail->next=head1;
+  head1=head1->next;
+  }
+
+  else{
+       Tail->next=head2;
+        head2=head2->next;
+  }
+  return head;
+}
