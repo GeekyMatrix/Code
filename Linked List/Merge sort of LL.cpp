7831f0f6de89a990a4cc75de7d8a980fb601f429
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-24 09:15:25 +0530

    Update Merge sort of LL.cpp

--- Linked List/Merge sort of LL.cpp
+++ Linked List/Merge sort of LL.cpp
@@ -1,3 +1,18 @@
+// Mid find karna
+Node *findMid(Node *head){
+ if(head==NULL ||head->next==NULL) return head;
+
+Node *slow=head;
+  Node *fast=head->next;
+
+while(fast!=NULL && fast->next!=NULL){
+slow=slow->next;
+  fast=fast->next->next;
+}
+return slow;
+}
+
+//Merge to Sorted LL
 Node *merge(Node *head1,Node *head2){
 
   if(head1==NULL) return head2;
