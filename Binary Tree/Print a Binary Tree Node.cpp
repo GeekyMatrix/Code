92876c9052545511505e793b1ce793d9d216c3ff
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-24 09:35:35 +0530

    Create Print a Binary Tree Node.cpp

---
+++ Binary Tree/Print a Binary Tree Node.cpp
@@ -0,0 +1,18 @@
+void printTree(Binary TreeNode<int>*root){
+//BASE CASE
+  if(root==NULL) return;
+
+  cout<<root->data<<":";
+  if(root->left!=NULL) cout<<"L"<<root->left->data;
+
+  if(root->right!=NULL) cout<<"R"<<root->right->data;
+
+ cout<<endl;
+  printTree(root->left);
+  printTree(root->right);
+}
+
+int main(){
+printTree(root);
+delete root;
+}
