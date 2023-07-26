05f16293432c9d6fa7ba922c7e90e7ec3d46d97f
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-07-26 13:11:42 +0530

    Update 1.cpp

--- Binary Search/1.cpp
+++ Binary Search/1.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int ans=-1;
+        int n=nums.size();
+        int s=0,e=n-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]==target) {
+                ans= mid;
+                return ans;
+            }
+            else if(nums[mid]<target) s=mid+1;
+            else e= mid-1;
+        }
+        return ans;
+    }
+};
