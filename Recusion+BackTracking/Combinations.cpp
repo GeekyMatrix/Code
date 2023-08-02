f9e1ca7db7596e176a3a61b57d59d9c6a239bf35
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-08-02 14:52:58 +0530

    Update Combinations.cpp

--- Recusion+BackTracking/Combinations.cpp
+++ Recusion+BackTracking/Combinations.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//Approach 1
 void solve(int index,int n,int k, vector<int>&output, vector<vector<int>>&ans){
     //BASE CASE
     if(k==0) {
@@ -17,6 +18,30 @@ void solve(int index,int n,int k, vector<int>&output, vector<vector<int>>&ans){
 }
 
     vector<vector<int>> combine(int n, int k) {
+         vector<vector<int>>ans;
+         vector<int>output;
+         solve(1,n,k,output,ans);
+         return ans;
+    }
+
+//Approach 2
+void solve(int index,int n,int k, vector<int>&output, vector<vector<int>>&ans){
+    //BASE CASE
+    if(k==0) {
+        ans.push_back(output);
+        return;
+    }
+
+   //INSERT THE ELEMENTS
+   for(int i=index;i<=n;i++){
+   output.push_back(i);
+   solve(i+1, n, k-1,output,ans);
+   //UNDO THE CHANGES
+   output.pop_back();
+   }
+
+}
+ vector<vector<int>> combine(int n, int k) {
          vector<vector<int>>ans;
          vector<int>output;
          solve(1,n,k,output,ans);
