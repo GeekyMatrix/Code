37725f3c7b0131f41eeade5bfdc8fe8446f3165c
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-08-13 17:40:29 +0530

    Update code.cpp

--- Graph/Cycle Detection In Undirected Graph by BFS/code.cpp
+++ Graph/Cycle Detection In Undirected Graph by BFS/code.cpp
@@ -1,1 +1,47 @@
+bool isCyclebfs(int src,unordered_map<int,bool>&visited,unordered_map<int,list<int>>&adj){
+  unordered_map<int,list<int>>parent;
+  parent[src]=-1;
+  visited[src]=1;
 
+  queue<int>q;
+  q.push(src);
+
+  while(!q.empty()){
+ int front=q.front;
+    q.pop();
+  //Neighbour pr Call karo
+    for(auto neighbour:adj[front]){ 
+      if(visited[neighbour]==true && neighbour!=parent[front]) return true;
+
+      else if(!visited[neighbour]){
+      q.push(neighbour);
+      visited[neighbour]=1;
+        parent[neighbour]=front;
+      }
+  }
+  }
+return false;
+}
+
+//Code ------->>>>>>>>>>>>>
+string cycleDetection(vector<vector<int>>edges,int n,int m){
+//Create Adjacency list
+  unordered_map<int,list<int>>adj;
+  for(int i=0;i<m;i++){
+  int u=edges[i][0];
+  int v=edges[i][1];
+
+  adj[u].push_back(v);
+  adj[v].push_back(u);
+  }
+//To handle disconnected components
+  unordered_map<int,bool>visited;
+
+  for(int i=0;i<n;i++){
+   if(!visited[i]){
+   bool ans=isCyclebfs(i,visited,adj);
+     if(ans==1) return "YES";
+   }
+  }
+return "No";
+}
