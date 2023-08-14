734862e8942203dfc876142a7389461eb49402ea
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-08-14 16:36:40 +0530

    Update Find Eventual Safe States.cpp

--- Graph/Cycle detection in Directed Graph By DFS/Find Eventual Safe States.cpp
+++ Graph/Cycle detection in Directed Graph By DFS/Find Eventual Safe States.cpp
@@ -1,30 +1,30 @@
- bool dfsRec(int node,vector<bool>&visited,vector<bool>&dfsvisited,vector<vector<int>>&graph,vector<bool>&present_cycle){
+ bool dfsRec(int node,vector<bool>&visited,vector<bool>&dfsvisited,vector<vector<int>>&graph){
       visited[node]=true;
       dfsvisited[node]=true;
       for(auto neighbour:graph[node]){ 
           if(!visited[neighbour]){
-              if(dfsRec(neighbour,visited,dfsvisited,graph,present_cycle)) { 
-                     return present_cycle[node]=true; 
+              if(dfsRec(neighbour,visited,dfsvisited,graph)) { 
+                     return true; 
                   }
           }
-        else if(visited[neighbour]&&dfsvisited[neighbour]) return present_cycle[node]=true;
+        else if(visited[neighbour]&&dfsvisited[neighbour]) return true;
       }
           dfsvisited[node]=false;
           return false;
   }
   //CODE
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int>ans;
         int n=graph.size();
         vector<bool>visited(n,false),dfsvisited(n,false);
-      vector<bool>present_cycle(n,false);
         for(int i=0;i<n;i++){ 
             if(!visited[i]){
-                dfsRec(i,visited,dfsvisited,graph,present_cycle);
+                dfsRec(i,visited,dfsvisited,graph);
             }
         }
+        
+         vector<int>safenodes;
         for(int i=0;i<n;i++){
-            if(!present_cycle[i]) ans.push_back(i);
+            if(dfsvisited[i]==false) safenodes.push_back(i);
         }
-        return ans;
+        return safenodes;
     }
