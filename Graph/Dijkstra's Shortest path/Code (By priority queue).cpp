1e0e4c8e354f06e813e62e727346ebc2ea9f036a
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-08-14 18:40:37 +0530

    Update Code (By priority queue).cpp

--- Graph/Dijkstra's Shortest path/Code (By priority queue).cpp
+++ Graph/Dijkstra's Shortest path/Code (By priority queue).cpp
@@ -8,7 +8,7 @@ class Solution
     	   //from the source vertex S.
 	    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) {
 
-		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;  // Creation of Min heap on basis(distance,node)
 
 		vector<int> result(V, INT_MAX);
 
@@ -16,18 +16,20 @@ class Solution
 		pq.push({0, S});
 
 		while(!pq.empty()) {
-
+                   // fetch top record
 		    int d  = pq.top().first;
 		    int node = pq.top().second;
+			//remove top record
 		    pq.pop();
 
+		//Traverse on neighbours
 		    for(auto &vec : adj[node]) {
 
 			int adjNode = vec[0];
 			int dist    = vec[1];
 
 			if(d + dist < result[adjNode]) {
-
+                     //distance update
 			    result[adjNode] = d + dist;
 			    pq.push({d+dist, adjNode});
 
