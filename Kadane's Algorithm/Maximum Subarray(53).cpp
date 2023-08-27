66c73e9d91854c844bd8a32c5260c4cde150b944
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-08-27 16:50:46 +0530

    Create Maximum Subarray(53).cpp

---
+++ Kadane's Algorithm/Maximum Subarray(53).cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+//TC=O(N^2)
+int maxSubArray(vector<int>& nums) {
+      int n=nums.size();
+        int max_sum=INT_MIN;
+
+  for(int i=0;i<n;i++){
+      int curr_sum=0;
+       for(int j=i;j<n;j++){
+           curr_sum+=nums[j];
+            if(curr_sum>max_sum) max_sum=curr_sum;
+       }
+  }
+  return max_sum;
+}
+
+//Kadane's Algorithm
+//TC=O(N)
+    int maxSubArray(vector<int>& nums) {
+      int n=nums.size();
+        int max_sum=INT_MIN;
+        int curr_sum=0;
+
+        for(int i=0;i<n;i++){
+          curr_sum+=nums[i];
+          if(curr_sum>max_sum) max_sum=curr_sum;
+             if(curr_sum<0) curr_sum=0;
+}
+return max_sum;
+        }
+ };
