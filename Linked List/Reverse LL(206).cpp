a2ff0003a77b36708f4bc1f0c19da2893221950a
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-01 11:16:05 +0530

    Update Reverse LL(206).cpp

--- Linked List/Reverse LL(206).cpp
+++ Linked List/Reverse LL(206).cpp
@@ -1,25 +1,29 @@
 //ITERATIVE APPROACH
- ListNode*reverseLL(ListNode*head){
-          ListNode *new_head=NULL;
-          while(head!=NULL){
-            ListNode *curr=head;
-            head=head->next;
-            curr->next=new_head;
-            new_head=curr;
-          } 
-         return new_head;
-     }
+class Solution {
+public:
+//Iterative method
+ ListNode* reverseList(ListNode* head) {
+ ListNode *new_head=NULL;
+ while(head!=NULL){
+     ListNode *temp=head;
+     head=head->next;
+     temp->next=new_head;
+     new_head=temp;
+ }
+  return new_head;
+ }
 
-//RECURSIVE METHOD
- ListNode*reverseLL(ListNode*head){
-         //BASE CASE
-         if(head==NULL||head->next==NULL) return head;
+//Recursive Method
+    ListNode* reverseList(ListNode* head) {
+        //Step :1 Base case
+        if(!head || !head->next) return head;
 
-         //Recursive call
-         ListNode*SmallAns=reverseLL(head->next);
-         //Calculation
-        head->next->next=head;
-         head->next=NULL;
-         return SmallAns;
-
-     }
+      //Step 2:Function call
+       ListNode* Chotahead = reverseList(head->next);  //1->2->3->NULL return krdega 3 ko
+       //lekin head mera 2 par hoga 
+       //step:3
+       head->next->next=head;  // 3->2
+       head->next=NULL;        //3->2->NULL
+       return Chotahead;       // 3 ko return krdia
+    }
+};
