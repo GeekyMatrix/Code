235ea115ae45aaeca039165f098fa87bd4c16c14
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-01 12:56:01 +0530

    Update Merge two Sorted LL.cpp

--- Linked List/Merge two Sorted LL.cpp
+++ Linked List/Merge two Sorted LL.cpp
@@ -1,43 +1,41 @@
-Node *merge(Node *head1,Node *head2){
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode*  head1, ListNode* head2) {
+        if(head1 == NULL) return head2;
+        if(head2 == NULL) return head1;
+       
+     //Step:1 intialize
+         ListNode *head=NULL;
+          ListNode *tail=NULL;
 
-  if(head1==NULL) return head2;
-   if(head2==NULL) return head1;
+          if(head1->val< head2->val){
+            head = head1;
+            tail = head1;
+            head1 = head1->next;
+          }
+           else{
+            head = head2;
+            tail = head2;
+            head2 = head2->next;
+           }
 
-  Node *head=NULL;
-  Node *Tail=NULL;
-
-  if(head1->data <head2->data){
-  head=head1;
-  Tail=head1;
-  head1=head1->next;
-  }
-
-else{
- head=head2;
-  Tail=head2;
-  head2=head2->next;
-}
-
-while(head1!=NULL && head2!=NULL){
- if(head1->data < head2->data){
- Tail->next=head1;
-  Tail=Tail->next;
-   head1=head1->next;
- }
-else{
- Tail->next=head2;
-  Tail=Tail->next;
-   head2=head2->next;
-}
-}
-  if(head1!=NULL){
- Tail->next=head1;
-  head1=head1->next;
-  }
-
-  else{
-       Tail->next=head2;
-        head2=head2->next;
-  }
-  return head;
-}
+        //Step:2 Move the pointer to the sorted list until both the LL exist
+          while(head1 !=NULL && head2 !=NULL){
+              if(head1->val<head2->val){
+                  tail->next =head1;
+                  tail =tail->next;
+                  head1 =head1->next;
+              }
+               else{
+                    tail->next =head2;
+                    tail =tail->next;
+                    head2 =head2->next;
+               }
+          }
+          //Step:3 if anyone is NULL then 
+              if(head1 !=NULL) tail->next =head1;
+              else tail->next =head2;
+             
+             return head;
+    }
+};
