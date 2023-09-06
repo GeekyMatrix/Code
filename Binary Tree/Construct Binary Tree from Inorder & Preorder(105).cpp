69f3f7e237c51c015d8ee149e1f32d90d2e96d2c
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-06 15:30:45 +0530

    Update and rename Construct Tree from Inorder & Preorder.cpp to Construct Binary Tree from Inorder & Preorder(105).cpp

--- Binary Tree/Construct Binary Tree from Inorder & Preorder(105).cpp
+++ Binary Tree/Construct Binary Tree from Inorder & Preorder(105).cpp
@@ -1,3 +1,30 @@
+//Approach 1
+class Solution {
+public:
+int preIndex=0;
+ TreeNode* solve(vector<int>& preorder,vector<int>& inorder,int is,int ie){
+ if(is>ie) return NULL;
+ TreeNode* root=new TreeNode(preorder[preIndex]);
+preIndex++;
+
+int inIndex;
+for(int i=is;i<=ie;i++){
+   if(inorder[i]==root->val){
+       inIndex=i;
+       break;
+   }
+}
+root->left=solve(preorder,inorder,is,inIndex-1);
+root->right=solve(preorder,inorder,inIndex+1,ie);
+
+return root;
+ }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* ans=solve(preorder,inorder,0,inorder.size()-1);
+        return ans;
+    }
+};
+//Approach 2
 class Solution{
     public:
     void createMapping(int in[], map<int,int> &nodeToindex,int n){
