2c198a130412197435bd1b966cd15e35694d46dd
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-08 23:53:45 +0530

    Update Code.cpp

--- Graph/DFS/Code.cpp
+++ Graph/DFS/Code.cpp
@@ -15,7 +15,7 @@ Vector<vector<int>depthfirstSearch(int V,int E,vector<vector,int>&edges){
   unordered_map<int,list<int>>adj;
   for(int i=0;i<edges.size();i++){
    int u=edges[i][0];
-    int v=edges[i][0];
+    int v=edges[i][1];
 
     adj[u].push_back(v);
     adj[v].push_back(u);
