db4d6898cb19cef7e283f09995d671b827968294
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-10 19:59:47 +0530

    Create Count All Valid Pickup and Delivery option(1359).cpp

---
+++ Combinatorics/Count All Valid Pickup and Delivery option(1359).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int M=1e9+7;
+    int countOrders(int n) {
+        if(n == 1) return 1;
+
+        long long res = 1; //for n==1
+
+        for(int i=2; i<=n; i++){
+        int spaces = (i-1)*2+1;
+
+         int possibility = spaces * (spaces + 1)/2;  
+
+         res *= possibility;
+          res %=M;
+        }
+        return  res;
+    }
+};
