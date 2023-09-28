f77084b200ec7f096cb5a98fa8f19b081f7d8369
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-28 17:50:00 +0530

    Create Leaf-Similar Trees(872).cpp

---
+++ Binary Tree/Leaf-Similar Trees(872).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void inorder(TreeNode* root, vector<int>&v){ //leaf nodes store krke rhega
+ if(root == NULL) return;
+ if(root->left == NULL && root->right == NULL) v.push_back(root->val);
+
+ inorder(root->left, v);
+ inorder(root->right, v);
+}
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>first,second;
+
+        inorder(root1,first);
+        inorder(root2,second);
+
+        if(first.size()!=second.size()) return false;
+    
+     for(int i = 0; i < first.size(); i++){
+      if(first[i]!=second[i]) return false;
+     } 
+     return true;
+    }
+};
