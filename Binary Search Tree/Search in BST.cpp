adfbac679e2b712122e4da54991236ffdbd8fbdf
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-09-30 18:07:14 +0530

    Delete Binary Search Tree/Search in BST.cpp

--- Binary Search Tree/Search in BST.cpp
+++
@@ -1,9 +0,0 @@
-bool SearchInBst(Binary TreeNode<int>*root,int k){
-if(root==NUL_ return false;
-
-  if(root->data==k) return true;
-
-  else if(k<root->data) return SearchInBst(root->left,k);
-
-  else return SearchInBst(root->right,k);
-}
