5ffa8a65594d2f5d6c43bcf6723b33d4b3bc32d3
Rohit kumar <97018533+GeekyMatrix@users.noreply.github.com>
2023-11-09 13:04:34 +0530

    Create Count Number of Homogenous Substrings(1759).cpp

---
+++ Mathematics/Count Number of Homogenous Substrings(1759).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int MOD = 1e9+7;
+    int countHomogenous(string s) {
+        int n = s.length();
+        int res = 0,cnt = 0;
+
+        for(int i = 0; i < n; i++){
+          if(i > 0 && s[i] == s[i - 1]) cnt++; //pichle wala bhi homogenoues character hai
+
+          else cnt = 1; // new character hoga
+
+          res = (res + cnt) % MOD;
+        }
+        return res;
+    }
+};
